e03c70c8fec715341a0116cf0daebe8bfc8ae0c6
Sandeepgompa <sandeepgompa01@gmail.com>
2025-01-12 10:28:39 +0530

    10-mysql-install.sh edited

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,6 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
 fi
 
 dnf install mysql -y
+
+dnf install git -y
