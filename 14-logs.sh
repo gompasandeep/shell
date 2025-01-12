84f2b2f8e4568eba210174bf0c4483329938846d
Sandeepgompa <sandeepgompa01@gmail.com>
2025-01-12 11:59:54 +0530

    14-logs.sh edited

---
+++ 14-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/logs/shellscript-logs" #to save logs in this folder
+LOGS_FILE=$( echo $0 | cut -d "." -f1 ) #echo $0 to print script name; 
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $? -ne 0 ] 
+        then
+            echo -e "$2 ... $R FAILURE"
+            exit 1
+        else
+            echo -e "$2 ... $G SUCCESS"
+        fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME 
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: You must have sudo access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing MySQL" 
+else
+    echo -e "MySQL is already ... $Y INSTALLED"
+fi
+
+
+dnf list installed git &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install Git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Git is already ... $Y INSTALLED"
+fi
