acb1de408b2bd6952f6ef53112db4b03efec1fc3
Sandeepgompa <sandeepgompa01@gmail.com>
2025-01-12 20:39:15 +0530

    17-backup.sh edited

--- 17-backup.sh
+++ 17-backup.sh
@@ -57,7 +57,7 @@ if [ -n "$FILES" ]
 then
     echo "Files are $FILES"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    find $SOURCE_DIR -name "*log" -mtime +$DAYS | zip -@ "ZIP_FILE"
+    find $SOURCE_DIR -name "*log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 else
     echo "No files found older than $DAYS to zip"
 fi
