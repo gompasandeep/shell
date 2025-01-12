fa1a94f6b1422d46f595fb78d7399644749f51c6
Sandeepgompa <sandeepgompa01@gmail.com>
2025-01-12 19:45:17 +0530

    17-backup.sh edited

---
+++ 17-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=$(3-14) # if user is not providing no. of days, we are taking 14 days as default
+
+LOGS_FOLDER="/var/logs/shellscript-logs" #to save logs in this folder
+LOGS_FILE=$( echo $0 | cut -d "." -f1 ) #echo $0 to print script name; 
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $? -ne 0 ] 
+        then
+            echo -e "$2 ... $R FAILURE $N"
+            exit 1
+        else
+            echo -e "$2 ... $G SUCCESS $N"
+        fi
+}
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 17-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"   
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
