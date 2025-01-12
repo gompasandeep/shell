0db950d472b98e27cc6a68f88e02ec3b1dfb63c9
Sandeepgompa <sandeepgompa01@gmail.com>
2025-01-12 19:00:48 +0530

    16-old-logs.sh edited

---
+++ 16-old-logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR="/home/ec2-user/shell/app-logs"
+
+LOGS_FOLDER="/var/logs/shellscript-logs" #to save logs in this folder
+LOGS_FILE=$( echo $0 | cut -d "." -f1 ) #echo $0 to print script name; 
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $? -ne 0 ] 
+        then
+            echo -e "$2 ... $R FAILURE $N"
+            exit 1
+        else
+            echo -e "$2 ... $G SUCCESS $N"
+        fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=(find $SOURCE_DIR -name "*log" -mtime +14)
+echo "Files to be deleted: $FILES_TO_DELETE
