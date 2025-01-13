c5367a01eb8e879b3677a553560b6878401c0fa7
Sandeepgompa <sandeepgompa01@gmail.com>
2025-01-13 21:08:12 +0530

    18-disk-usage.sh

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -6,6 +6,6 @@ DISK_THRESHOLD=5
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk -F " " '{print $6F}')
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "Partition: $PARTITION, Usage: $USAGE"
 done <<< $DISK_USAGE
