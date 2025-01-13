a160808e9eefeeae4bf6066677a365c80adb318a
Sandeepgompa <sandeepgompa01@gmail.com>
2025-01-13 21:07:22 +0530

    18-disk-usage.sh

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
 
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $6F}')
-    echo "Partition: $PARTITION, Usage $USAGE"
+    echo "Partition: $PARTITION, Usage: $USAGE"
 done <<< $DISK_USAGE
